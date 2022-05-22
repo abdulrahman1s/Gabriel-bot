73e6ee27d40944501097e8ca50e0265f65af8d1c
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-22 19:20:10 +0200

    feat: Remove bad words detection feature

--- src/events/guildUpdate.ts
+++ src/events/guildUpdate.ts
@@ -1,46 +1,20 @@
 import type { Guild } from 'discord.js'
-import { BAD_WORDS } from '../Constants'
-import { isInvite } from '../utils'
-
-const ARABIC_LETTERS_REPLACER = [
-    ['أ', 'ا'],
-    ['إ', 'ا'],
-    ['ٌ', ''],
-    ['ُ', ''],
-    ['ً', ''],
-    ['َ', ''],
-    ['ٍ', ''],
-    ['ْ', ''],
-    ['ّ', ''],
-    ['ِ', '']
-].map(([search, replace]) => [RegExp(search, 'g'), replace]) as [RegExp, string][]
+import { inviteRegExp } from '../utils'
 
 export const guildUpdate = async (oldGuild: Guild, guild: Guild): Promise<void> => {
+    if (!guild.active) return
+
     if (guild.defaultMessageNotifications === 'ALL_MESSAGES') {
-        await guild.setDefaultMessageNotifications('ONLY_MENTIONS', 'Auto disable all-messages notifications mode').catch(() => null)
+        await guild.setDefaultMessageNotifications('ONLY_MENTIONS', 'All Messages cannot be enabled').catch(() => null)
     }
 
-    if (guild.name === oldGuild.name) return
+    if (guild.name === oldGuild.name || !inviteRegExp.test(guild.name)) return
 
-    const { executor } = (await guild.fetchEntry('GUILD_UPDATE')) ?? {}
+    const executor = await guild.fetchExecutor('GUILD_UPDATE')
 
-    if (executor && !guild.client.isPunishable(executor.id)) return
+    if (executor && !guild.isPunishable(executor.id)) return
 
-    let name = guild.name
-
-    // Clean up special arabic letters
-    for (const args of ARABIC_LETTERS_REPLACER) name = name.replace(...args)
-
-    let has = false
-
-    if (isInvite(name)) has = true
-    else for (const word of name.split(' ')) if (BAD_WORDS.includes(word.toLowerCase())) {
-        has = true
-        break
-    }
-
-    if (has) {
-        await guild.setName(oldGuild.name, `(${executor?.tag ?? 'Unknown#0000'}): Detected bad name`)
-        if (executor) await guild.punish(executor.id)
-    }
+    await guild.setName(oldGuild.name, `(${executor?.tag ?? 'Unknown#0000'}): This name cantinas an invite`)
+    
+    if (executor) await guild.punish(executor.id)
 }
