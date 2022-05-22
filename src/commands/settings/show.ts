6743be72d004153c6f8c132ec0066911047a05fb
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-22 20:11:02 +0200

    feat(commands): Add invite command

--- src/commands/settings/show.ts
+++ src/commands/settings/show.ts
@@ -2,7 +2,6 @@ import type { CTX, SubCommand } from '../../structures'
 import { MessageEmbed } from 'discord.js'
 import ms from 'ms'
 
-const humanizeBoolean = (x: unknown) => x ? '**Yes**' : '**No**'
 
 export class SettingsShowCommand implements SubCommand {
     name = 'show'
@@ -12,13 +11,13 @@ export class SettingsShowCommand implements SubCommand {
         const { privateAlerts, limits, ignoredIds } = ctx.guild.settings
         const embed = new MessageEmbed().setTitle('Guild Settings').setColor('BLURPLE')
 
-        embed.addField('Private Alerts:', humanizeBoolean(privateAlerts))
+        embed.addField('Private Alerts:', privateAlerts ? '**Enabled**' : '**Disabled**')
         embed.addField('Limits:', [
             `Create **${limits.create.max}** / **${ms(limits.create.time)}**`,
             `Update **${limits.delete.max}** / **${ms(limits.delete.time)}**`,
             `Delete **${limits.update.max}** / **${ms(limits.update.time)}**`
         ].join('\n'))
-        embed.addField('Trusted Users:', ignoredIds.map(id => `<@${id}>`).join(', '))
+        embed.addField('Trusted Users:', ignoredIds.map(id => `<@${id}>`).join(', ') || 'None')
 
         return ctx.reply({ embeds: [embed], ephemeral: true })
     }
