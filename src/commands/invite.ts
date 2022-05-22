fc541ad79f2944f756d98979ba81394fffcd8661
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-05-22 20:11:02 +0200

    feat(commands): Add invite command

---
+++ src/commands/invite.ts
@@ -0,0 +1,13 @@
+import type { CTX, Command } from '../structures'
+
+export class InviteCommand implements Command {
+    name = 'invite'
+    description = 'An invite to add me to your server'
+    run(ctx: CTX) {
+        const link = ctx.client.generateInvite({
+            scopes: ['bot', 'applications.commands'],
+            permissions: ['ADMINISTRATOR']
+        })
+        return ctx.reply(`[Invite me!](${link})`)
+    }
+}
--- src/extensions/GuildMember.ts
+++ src/extensions/GuildMember.ts
@@ -3,11 +3,11 @@ import { GuildMember } from 'discord.js'
 GuildMember.prototype.dm = async function (content: string, times = 1) {
     if (!this.guild.settings.privateAlerts) return false
 
-    let i = 0
-
     const channel = await this.createDM(false).catch(() => null)
 
     if (!channel) return false
+
+    let i = 0
 
     while (i++ < times) try {
         await channel.send(content)
