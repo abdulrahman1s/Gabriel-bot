9a4509b2fc8f1c52e2d67c451de9c04fc4cdd1ca
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-23 01:17:45 +0200

    feat: Change embed colors & look

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -6,7 +6,7 @@ export class AboutCommand implements Command {
     name = 'about'
     description = 'Show information about the bot'
     run(ctx: CTX) {
-        const embed = new MessageEmbed().setColor('BLURPLE'), row = new MessageActionRow()
+        const embed = new MessageEmbed().setColor('#2f3136'), row = new MessageActionRow()
 
         const button = () => new MessageButton().setStyle('LINK')
 
@@ -15,7 +15,7 @@ export class AboutCommand implements Command {
             .setThumbnail(ctx.client.user.displayAvatarURL())
             .setDescription('The ultimate all-in-one protection discord bot you\'ll ever need, really')
 
-        embed.addField('The name', `**Gabriel** (**جبريل**romanized: *Jibrīl*) is is venerated as one of the primary archangels and as the Angel of Revelation in Islam. [More info...](https://en.wikipedia.org/wiki/Gabriel)`)
+        embed.addField('The name?', `**Gabriel** (Arabic: **جبريل** romanized: *Jibrīl*) is is venerated as one of the primary archangels and as the Angel of Revelation in Islam. [More info...](https://en.wikipedia.org/wiki/Gabriel)`)
 
         const inviteLink = ctx.client.generateInvite({
             scopes: ['bot', 'applications.commands'],
@@ -23,7 +23,7 @@ export class AboutCommand implements Command {
         })
 
         row.addComponents(
-            button().setURL('https://ko-fi.com/abdulrahman1s').setLabel('Donation').setEmoji('🎔'),
+            button().setURL('https://ko-fi.com/abdulrahman1s').setLabel('Donation').setEmoji('❤️'),
             button().setURL('https://github.com/abdulrahman1s/Anti-Raid').setLabel('Source Code').setEmoji('💻'),
             button().setURL(inviteLink).setLabel('Invite').setEmoji('🔗')
         )
