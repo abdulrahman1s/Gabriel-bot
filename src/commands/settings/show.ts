c6ee4cc1081f6954df928ffad801d95a6677d551
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-05-23 03:40:58 +0200

    feat: Show snapshot status

--- src/commands/settings/show.ts
+++ src/commands/settings/show.ts
@@ -8,16 +8,18 @@ export class SettingsShowCommand implements SubCommand {
     type = 'SUB_COMMAND' as const
     description = 'Show current settings'
     run(ctx: CTX) {
-        const { privateAlerts, limits, ignoredIds } = ctx.guild.settings
+        const { privateAlerts, limits, ignoredIds, snapshots } = ctx.guild.settings
         const embed = new MessageEmbed().setTitle('Server Settings').setColor('#2f3136')
 
-        embed.addField('Limits:', [
-            `Create **${limits.create.max}** / **${ms(limits.create.time)}**`,
-            `Update **${limits.update.max}** / **${ms(limits.update.time)}**`,
-            `Delete **${limits.delete.max}** / **${ms(limits.delete.time)}**`
-        ].join('\n'), true)
-        embed.addField('Private Alerts:', privateAlerts ? '**Enabled**' : '**Disabled**', true)
-        embed.addField('Trusted Users:', ignoredIds.map(id => `<@${id}>`).join(', ') || 'None')
+        embed
+            .addField('Limits:', [
+                `Create **${limits.create.max}** / **${ms(limits.create.time)}**`,
+                `Update **${limits.update.max}** / **${ms(limits.update.time)}**`,
+                `Delete **${limits.delete.max}** / **${ms(limits.delete.time)}**`
+            ].join('\n'), true)
+            .addField('Private Alerts:', privateAlerts ? '**Enabled**' : '**Disabled**', true)
+            .addField('Snapshots:', snapshots.enabled ? '**Enabled**' : '**Disabled**', true)
+            .addField('Trusted Users:', ignoredIds.map(id => `<@${id}>`).join(', ') || 'None')
 
         return ctx.reply({ embeds: [embed] })
     }
