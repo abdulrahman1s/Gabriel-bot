422e157a1456e0e4b1e9e294527321dfcd9e4976
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-05-23 02:09:43 +0200

    feat: Set status to mobile

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,15 @@ const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        // Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_WEBHOOKS
     ],
     restTimeOffset: 0,
-    presence: {
-        status: 'invisible'
+    ws: {
+        properties: {
+            $browser: 'Discord iOS'
+        }
     }
 })
 
