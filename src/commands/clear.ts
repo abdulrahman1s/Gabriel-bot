3f4186279a0d286b2f46be9d64706fbcb77fe8fe
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-05-23 02:43:37 +0200

    feat(commands#clear): Add filter option

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -1,5 +1,6 @@
 import { CTX, Command, CommandError } from '../structures'
 import { setTimeout as sleep } from 'timers/promises'
+import { createChoices, inviteRegExp } from '../utils'
 
 export class ClearCommand implements Command {
     name = 'clear'
@@ -9,16 +10,34 @@ export class ClearCommand implements Command {
         type: 'INTEGER' as const,
         description: 'Number of messages you want to delete (default: 50)',
         required: false
+    }, {
+        name: 'filter',
+        type: 'STRING' as const,
+        description: 'filter messages based on it\'s content',
+        choices: createChoices(['attachments', 'embeds', 'invites', 'bots', 'humans', 'links']),
+        required: false
     }]
-
     async run(ctx: CTX) {
         const count = ctx.options.getInteger('count') ?? 50
+        const filter = ctx.options.getString('filter')
 
         if (count > 100 || count <= 0) throw new CommandError('Number of messages must be lower than `100` and greater than `0`.')
 
-        const deletedMessages = await ctx.channel.bulkDelete(count)
+        let messages = await ctx.channel.messages.fetch({ limit: count })
+
+        switch (filter) {
+            case 'bots': messages = messages.filter(m => m.author.bot); break
+            case 'humans': messages = messages.filter(m => !m.author.bot); break
+            case 'embeds': messages = messages.filter(m => m.embeds.length > 0); break
+            case 'attachments': messages = messages.filter(m => m.attachments.size > 0); break
+            case 'invites': messages = messages.filter(m => inviteRegExp.test(m.content)); break
+            case 'links': messages = messages.filter(m => /https?:\/\//i.test(m.content)); break
+            default: break
+        }
+
+        const deletedMessages = await ctx.channel.bulkDelete(messages)
         const msg = await ctx.reply({ content: `**${deletedMessages.size}** of messages has been deleted`, fetchReply: true })
 
-        return sleep(3000).then(msg.delete)
+        return sleep(3000).then(() => msg.delete())
     }
 }
