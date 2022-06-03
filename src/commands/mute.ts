21c35770234ac22e8287ae406e985b60aac4b0b3
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-06-03 20:41:59 +0200

    fix: Show mute duration correctly

--- src/commands/mute.ts
+++ src/commands/mute.ts
@@ -25,13 +25,13 @@ export class MuteCommand implements Command {
     async run(ctx: CTX) {
         const member = ctx.options.getMember('user', true)
         const reason = ctx.options.getString('reason') || 'No reason'
-        const duration = Date.now() + ms((ctx.options.getString('duration') || '30 minute') as StringValue)
+        const duration = ms((ctx.options.getString('duration') || '30 minute') as StringValue)
 
         if (member.id === ctx.client.user.id) throw new CommandError('You can\'t mute me')
         if (member.id === ctx.user.id) throw new CommandError('You can\'t mute yourself')
         if (isNaN(duration)) throw new CommandError('Invalid duration input')
 
-        await member.disableCommunicationUntil(duration, reason)
+        await member.disableCommunicationUntil(Date.now() + duration, reason)
 
         const formattedDuration = ms(duration, { long: true })
 
